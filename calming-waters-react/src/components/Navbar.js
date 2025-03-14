7504f1c8c53f3a546c033fad30a51af1c831377e
Kawika Ilac <kawikailac22@gmail.com>
2025-03-13 19:07:42 -0600

    i need a drink

--- calming-waters-react/src/components/Navbar.js
+++ calming-waters-react/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
@@ -6,37 +6,42 @@ import "./Navbar.css";
 
 const Navbar = () => {
   const location = useLocation(); // Get current page
-
-  const navItems = [
-    { name: "Home", path: "/" },
-    { name: "Services", path: "/services" },
-    { name: "About", path: "/About" },
-    { name: "Contact", path: "/contact" }
-  ];
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid"> {/* Change this from "container" to "container-fluid" */}
-    {/* Logo & Brand */}
-    <a className="navbar-brand d-flex align-items-center" href="#">
+      <div className="container-fluid">
+        {/* Logo & Brand */}
+        <Link to="/" className="navbar-brand d-flex align-items-center">
           <img src="/calming-waters-pics/logo.png" alt="Logo" className="nav-logo" />
           Calming Waters
-        </a>
-    <a className="navbar-brand" href="#"></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav ms-auto">
-        <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-    
-        <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
+        </Link>
 
-        <li className="nav-item"><Link className="nav-link" to="/schedule">Schedule</Link></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        {/* Desktop Navigation */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/schedule">Schedule</Link></li>
+          </ul>
+        </div>
+
+        {/* 🔹 Mobile Dropdown Menu */}
+        <div className="dropdown-container">
+          <button className="dropdown-toggle" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+            ☰ {/* Mobile Menu Icon */}
+          </button>
+
+          {/* Dropdown Menu */}
+          {isDropdownOpen && (
+            <div className="navbar-dropdown">
+              <Link className="dropdown-item" to="/about" onClick={() => setIsDropdownOpen(false)}>About</Link>
+              <Link className="dropdown-item" to="/schedule" onClick={() => setIsDropdownOpen(false)}>Schedule</Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 };
 
